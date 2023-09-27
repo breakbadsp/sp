3e8d9114796fa330209efd27352c51739a523c86
Sachin Pandhare <sachinadcet@gmail.com>
2023-09-27 21:42:34 +0530

    CMN | Test cases added for CreateThreadAndRun

---
+++ tests/CMN.cpp
@@ -0,0 +1,24 @@
+#include "CMN.hpp"
+#include "cmn.hpp"
+
+int CalculateSum(int a, int b, int c)
+{
+    return a + b + c;
+}
+
+void TestCmn()
+{
+    auto* new_thread = CreateAndRunThread(
+            0, 
+            "TestThread",
+            CalculateSum,
+            10, 20, 30
+            );
+
+    if(!new_thread)
+        return;
+
+    new_thread->join();
+    std::cout << "Exiting main\n";
+    delete new_thread;   
+}
--- tests/unit_test_main.cpp
+++ tests/unit_test_main.cpp
@@ -5,19 +5,30 @@
 #include "QueueTest.hpp"
 #include "TestSharedPtr.hpp"
 #include "MapTest.hpp"
+#include "CMN.hpp"
+#include "cmn.hpp"
 
 int main(int argc, char** argv)
 {
+  std::vector<std::thread*> threads;
+  TestCmn();
+  
   if(argc <= 1)
-    TestQueue(1);
+    threads.emplace_back(CreateAndRunThread(-1, "TestQueue", TestQueue, 1));
   else
-    TestQueue(atoi(argv[1]));
-  TestVectorContructor();
-  TestVectorPushBack();
-  TestListApis();
-  TestString();
-  TestSharedPtr();
-  TestMap();
+    threads.emplace_back(CreateAndRunThread(0, "TestQueue", TestQueue, atoi(argv[1])));
 
+  threads.emplace_back(CreateAndRunThread(1, "TestVectorPushBack", TestVectorPushBack));
+  threads.emplace_back(CreateAndRunThread(2, "TestListApis", TestListApis));
+  threads.emplace_back(CreateAndRunThread(3, "TestString", TestString));
+  threads.emplace_back(CreateAndRunThread(4, "TestSharedPtr", TestSharedPtr));
+  threads.emplace_back(CreateAndRunThread(5, "TestMap", TestMap));
+
+  for(auto* t: threads)
+  {
+    t->join();
+    delete t;
+  }
+  
   std::cout << "test are done\n";
 }
