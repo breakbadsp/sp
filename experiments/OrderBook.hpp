8819420c63e81c9632c86bff98856832519bd2b1
Sachin Pandhare <sachinadcet@gmail.com>
2023-09-30 20:33:40 +0530

    OrderBook | Improved order book to add matching, BID Size, ASK size and fixed issues

--- experiments/OrderBook.hpp
+++ experiments/OrderBook.hpp
@@ -1,9 +1,17 @@
+#pragma once
+
 #include <set>
 #include <map>
 #include <unordered_map>
 #include <string>
 #include <functional>
 #include <iostream>
+
+#include <sys/time.h>
+
+#include "cmn.hpp"
+
+constexpr size_t TICK_SIZE = 2; //two decimal places, 1 paisa tick size
 
   enum class Side
   {
@@ -13,6 +21,7 @@
 
 struct OrderEvent
 {
+public:
   enum class Type
   {
     New,
@@ -20,65 +29,112 @@ struct OrderEvent
     Cxl
   };
 
-  Type type_ {Type::New};
-  std::string order_id_;
-  std::string symbol_;
-  int size_ {0};
-  double price_ {0};
-  Side side_ {Side::Buy};
+  OrderEvent( int p_id, 
+              const std::string& p_symbol,
+              double p_price,
+              int p_size,
+              Type p_type,
+              Side p_side)
+  : id_(p_id)
+  , symbol_(p_symbol)
+  , price_(p_price)
+  , size_(p_size)
+  , type_(p_type)
+  , side_(p_side)
+  {}
 
+  void Clear()
+  {
+    id_ = 0;
+    symbol_.clear();
+    price_ = 0;
+    size_ = {0};
+    type_ = {Type::New};
+    side_ = {Side::Buy};
+  }
+
+public:
+  int id_ {0};
+  std::string symbol_;
+  double price_ {0};
+  int size_ {0};
+  Type type_ {Type::New};
+  Side side_ {Side::Buy};
 };
 
 struct Order
 {
+public:
   Order(const OrderEvent& p_event)
-  {
-    FromEvent(p_event);
+    : id_(p_event.id_)
+    , price_(p_event.price_)
+    , size_(p_event.size_)
+    , side_(p_event.side_)
+  { 
+    gettimeofday(&time_, nullptr);
   }
 
-  void FromEvent(const OrderEvent& p_event)
+  //void FromEvent(const OrderEvent& p_event)
+
+  const int id_;
+  const double price_{0};
+  mutable int size_{0};
+  struct timeval time_;
+  const Side side_ {Side::Buy};
+
+  bool operator<(const Order &p_rhs) const
   {
-    price_ = p_event.price_;
-    size_ = p_event.size_;
-    time_ = time(0);
-    id_ = p_event.order_id_;
-    side_ = p_event.side_;
+    if(time_.tv_sec < p_rhs.time_.tv_sec)
+      return true;
+    else if(time_.tv_sec > p_rhs.time_.tv_sec)
+      return false;
+
+    return time_.tv_usec < p_rhs.time_.tv_usec;
   }
 
-  double price_{0};
-  int size_{0};
-  int time_{0};
-  std::string id_;
-  Side side_ {Side::Buy};
-
-  bool operator<(const Order &p_rhs)
+  void Print() const
   {
-    return time_ < p_rhs.time_;
+    std::cout 
+      << id_  << ", "
+      << price_  << ", "
+      << size_  << ", "
+      << time_.tv_sec << ":"
+      << time_.tv_usec << ", "
+      << (int)side_  << std::endl;
   }
 };
 
 class Level
 {
 public:
-  int size_{0};
-  double price_{0};
-  std::multiset<Order> orders_;
-
-  bool AddOrder(Order&& p_order)
+  Level(Order p_order)
   {
-    std::cout << "Order id " << p_order.id_ << " Added\n";
+    std::cout << "Order " << p_order.id_ 
+      << " Added to new level, price:" << p_order.price_
+      << '\n' ;
+
     size_ += p_order.size_;
-    orders_.insert(std::move(p_order));
+    price_ = p_order.price_;
+    orders_.insert(p_order);
+  }
+
+  bool AddOrder(Order p_order)
+  {
+    std::cout << "Order id " << p_order.id_ << " Added to price level " << price_ << "\n";
+    size_ += p_order.size_;
+    orders_.insert(p_order);
     return true;
   }
 
   bool CancelOrder(const Order& p_order) //FIXME:: optimize
   {
+    //TODO:: Handle Level deletion
+    //If level removed is TOB, try matching
     for(auto itr = orders_.begin(); itr != orders_.end(); ++itr)
     {
       if(itr->id_ == p_order.id_)
       {
-        std::cout << "Order id " << p_order.id_ << " Cancelled\n";
+        std::cout << "Order id " << p_order.id_ << " removed from price level " << price_ << "\n";
         size_ -= itr->size_;
         itr = orders_.erase(itr);
         break;
@@ -87,12 +143,12 @@ public:
     return true;
   }
 
-  bool Match(const Order& p_order)
+  bool Match(Order p_order)
   {
-    std::cout << "Order id " << p_order.id_ << " matched\n";
     int remaining_qty = p_order.size_;
-    for(auto itr = orders_.begin(); itr != orders_.end(); ++itr)
+    for(auto itr = orders_.begin(); itr != orders_.end();)
     {
+      std::cout << "Order " << p_order.id_ << " matched with " << itr->id_ << "\n";
       size_ -= itr->size_;
       if(itr->size_ < remaining_qty)
       {
@@ -112,32 +168,62 @@ public:
     }
     return true;
   }
+
+  int get_size() const { return size_; }
+  double get_price() const { return price_; }
+
+protected:
+  int size_{0};
+  double price_{0};
+  std::set<Order> orders_; //it is sorted by time
 };
 
 class BuyLevel : public Level
 {
 public:
-  const Side side_{Side::Buy};
+  Side side_{Side::Buy};
 
   bool operator<(const BuyLevel& p_rhs)
   {
-    return price_ > p_rhs.price_;
+    return get_price() > p_rhs.get_price();
   }
 };
 
 class SellLevel : public Level
 {
 public:
-  const Side side_{Side::Sell};
+  Side side_{Side::Sell};
 
   bool operator<(const BuyLevel& p_rhs)
   {
-    return price_ < p_rhs.price_;
+    return get_price() < p_rhs.get_price();
   }
 };
 
 class OrderBook
 {
+public:
+
+  OrderBook(const std::string& p_symbol)
+  : symbol_(p_symbol)
+  {}
+
+  ~OrderBook()
+  {
+    std::cout << "Deleting order book \n";
+  }
+
+  void Print() const
+  {
+    std::cout << "==========\nOrder book " 
+      << " Symbol:" << symbol_
+      << " LTP:" << get_last_traded_price()
+      << " BIDprice:" << get_bid_price()
+      << " ASKprice:" << get_ask_price()
+      << " BIDSize:" << get_bid_size()
+      << " ASKSize:" << get_ask_size() << "\n============\n";
+  }
+
   static bool Process(const OrderEvent& p_event)
   {
     Order order(p_event);
@@ -148,11 +234,13 @@ class OrderBook
         auto itr = s_order_book_by_symbol.find(p_event.symbol_);
         if(itr != s_order_book_by_symbol.end())
         {
-          return itr->second->AddOrder(std::move(order));
+          itr->second->Print();
+          return itr->second->AddOrder((order));
         }
-        OrderBook* new_book = new OrderBook();
-        s_order_book_by_symbol.insert_or_assign(p_event.symbol_, new_book);
-        return new_book->AddOrder(std::move(order)); 
+        auto new_book = std::make_unique<OrderBook>(p_event.symbol_);
+        auto* ptr = new_book.get();
+        s_order_book_by_symbol.insert_or_assign(p_event.symbol_, std::move(new_book));
+        return ptr->AddOrder((order));   
       }
       break;
 
@@ -186,34 +274,132 @@ class OrderBook
     default:
       break;
     }
+    return false;
+  }
+
+  double get_bid_price() const
+  {
+    if(!BIDS_.empty())
+      return ( BIDS_.cbegin()->first);
+    return -1;
+  }
+
+  double get_ask_price() const
+  {
+    if(!ASKS_.empty())
+      return ( ASKS_.cbegin()->first);
+    return -1;
+  }
+
+  double get_bid_size() const
+  {
+    if(!BIDS_.empty())
+      return ( BIDS_.cbegin()->second.get_size());
+    return -1;
+  }
+
+  double get_ask_size() const
+  {
+    if(!ASKS_.empty())
+      return ( ASKS_.cbegin()->second.get_size());
+    return -1;
+  }
+
+  double get_last_traded_price() const
+  {
+    return last_traded_price_;
   }
 
   private:
+    //TODO:: Convert BIDS and ASKS to heap
     std::map<double, BuyLevel, std::greater<double>> BIDS_;
     std::map<double, SellLevel, std::less<double>> ASKS_;
-    static std::unordered_map<std::string, OrderBook*> s_order_book_by_symbol;
+    double last_traded_price_ {0};
+    std::string symbol_;
+    inline static std::unordered_map<std::string, std::unique_ptr<OrderBook>> s_order_book_by_symbol {};
 
     bool Match(const Order& p_order)
     {
       switch (p_order.side_)
       {
+      case Side::Buy:
+        {
+          if(sp::almost_equal(get_ask_price(), p_order.price_, TICK_SIZE))
+          {
+            last_traded_price_ = p_order.price_;
+            ASKS_.begin()->second.Match(p_order);
+            return true;
+          }
+          return false;
+        }
+        break;
       
+      case Side::Sell:
+        {
+          if(sp::almost_equal(get_bid_price(), p_order.price_, TICK_SIZE))
+          {
+            last_traded_price_ = p_order.price_;
+            BIDS_.begin()->second.Match(p_order);
+            return true;
+          }
+          return false;
+        }
+        break;
+
+      default:
+        break;
+      }
+
+      return true;
+    }
+
+    bool AddOrder(Order p_order)
+    {
+      std::cout << "Adding below order to order book :\n";
+      p_order.Print();
+
+      if(Match(p_order))
+        return true;
+      
+      switch (p_order.side_)
+      {
+        
       case Side::Buy:
       {
-        auto itr = ASKS_.find(p_order.price_);
-        if(itr != ASKS_.end())
+        auto itr = BIDS_.find(p_order.price_);
+        if(itr == BIDS_.end())
         {
-          itr->second.Match(p_order);
+          //Create Level
+          if( !(BIDS_.emplace(p_order.price_, p_order)).second )
+          {
+            std::cerr << "Fatal order insertion in order book failed\n";
+            return false;
+          }
+          return true;
+        }
+        if(!itr->second.AddOrder(p_order))
+        {
+          return false;
         }
         break;
       }
 
       case Side::Sell:
       {
-        auto itr = BIDS_.find(p_order.price_);
-        if(itr != BIDS_.end())
+        auto itr = ASKS_.find(p_order.price_);
+        if(itr == ASKS_.end())
         {
-          itr->second.Match(p_order);
+          //Create Level
+          if( !(ASKS_.emplace(p_order.price_, p_order).second ) )
+          {
+            std::cerr << "Fatal order insertion in order book failed\n";
+            return false;
+          }
+          return true;
+        }
+        if(!itr->second.AddOrder(p_order))
+        {
+          return false;
         }
         break;
       }
@@ -222,48 +408,6 @@ class OrderBook
         break;
       }
       return false;
-    }
-
-    bool AddOrder(Order&& p_order)
-    {
-      if(Match(p_order))
-        return true;
-      
-      switch (p_order.side_)
-      {
-      
-      case Side::Buy:
-      {
-        auto itr = BIDS_.find(p_order.price_);
-        if(itr != BIDS_.end())
-        {
-          return itr->second.AddOrder(std::move(p_order));
-        }
-        auto price = p_order.price_;
-        BuyLevel new_buy_level;
-        new_buy_level.price_ = p_order.price_;
-        new_buy_level.AddOrder(std::move(p_order));
-        BIDS_.insert_or_assign(price, std::move(new_buy_level));
-        return true;
-      }
-
-      case Side::Sell:
-      {
-        auto itr = ASKS_.find(p_order.price_);
-        if(itr != ASKS_.end())
-        {
-          return itr->second.AddOrder(std::move(p_order));
-        }
-        SellLevel new_level;
-        new_level.price_ = p_order.price_;
-        new_level.AddOrder(std::move(p_order));
-        ASKS_.insert_or_assign(p_order.price_, std::move(new_level));
-        return true;
-      }
-
-      default:
-        return false;
-      }
     }
 
   
@@ -310,7 +454,7 @@ class OrderBook
         auto itr = BIDS_.find(p_order.price_); 
         if(itr != BIDS_.end())
         {
-          return (itr->second.CancelOrder(p_order) && itr->second.AddOrder(std::move(p_order)));
+          return (itr->second.CancelOrder(p_order) && itr->second.AddOrder((p_order)));
         }
         return false;
       }
--- tests/unit_test_main.cpp
+++ tests/unit_test_main.cpp
@@ -7,11 +7,40 @@
 #include "MapTest.hpp"
 #include "CMN.hpp"
 #include "cmn.hpp"
+#include "OrderBook.hpp"
 
 int main()
 {
   std::vector<std::thread*> threads;
   TestCmn();
+
+
+  std::cout << "Testing order book\n";
+
+  int order_id = 0;
+
+  OrderBook::Process({order_id++, "REL", 99.01, 100, OrderEvent::Type::New, Side::Buy});
+  OrderBook::Process({order_id++, "REL", 99.01, 100, OrderEvent::Type::New, Side::Sell});
+
+  OrderBook::Process({order_id++, "REL", 99.01, 100, OrderEvent::Type::New, Side::Buy});
+  OrderBook::Process({order_id++, "REL", 99.01, 200, OrderEvent::Type::New, Side::Buy});
+  OrderBook::Process({order_id++, "REL", 99.01, 300, OrderEvent::Type::New, Side::Buy});
+  OrderBook::Process({order_id++, "REL", 99.01, 300, OrderEvent::Type::New, Side::Sell});
+
+  OrderBook::Process({order_id++, "REL", 99.02, 100, OrderEvent::Type::New, Side::Buy} );
+  OrderBook::Process({order_id++, "REL", 99.03, 100, OrderEvent::Type::New, Side::Buy} );
+  OrderBook::Process({order_id++, "REL", 99.04, 100, OrderEvent::Type::New, Side::Buy} );
+  OrderBook::Process({order_id++, "REL", 99.05, 100, OrderEvent::Type::New, Side::Buy} );
+  
+  OrderBook::Process({order_id++, "REL", 99.06, 100, OrderEvent::Type::New, Side::Sell} );
+  OrderBook::Process({order_id++, "REL", 99.07, 100, OrderEvent::Type::New, Side::Sell} );
+  OrderBook::Process({order_id++, "REL", 99.08, 100, OrderEvent::Type::New, Side::Sell} );
+  OrderBook::Process({order_id++, "REL", 99.09, 100, OrderEvent::Type::New, Side::Sell} );
+
+  OrderBook::Process({order_id++, "REL", 99.05, 100, OrderEvent::Type::New, Side::Sell} );
+  OrderBook::Process({order_id++, "REL", 99.04, 100, OrderEvent::Type::New, Side::Sell} );
+  OrderBook::Process({order_id++, "REL", 99.06, 100, OrderEvent::Type::New, Side::Buy} );
+
   
   using namespace sp;
   //if(argc <= 1)
