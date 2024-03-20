f5d870a06132fba0efea57728b6cb10fc8d2fe73
Sachin Pandhare <sachinadcet@gmail.com>
2024-03-20 23:51:09 +0530

    Spinlock fixed

--- sp/spinlock.hpp
+++ sp/spinlock.hpp
@@ -2,9 +2,6 @@
 
 namespace sp
 {
-  bool LOCKED = true;
-  bool UNLOCKED = false;
-
   struct poor_mutex
   {
     int flag = 0;
@@ -39,6 +36,9 @@ namespace sp
 
   struct spinlock_v2
   {
+    bool LOCKED = true;
+    bool UNLOCKED = false;
+
     public:
       spinlock_v2() = default;
       spinlock_v2(const spinlock_v2&) = delete;
@@ -46,18 +46,18 @@ namespace sp
       spinlock_v2 operator=(const spinlock_v2&) = delete;
       spinlock_v2 operator=(spinlock_v2&&) = delete;
 
-      void lock() const
+      void lock()
       {
         while(!std::atomic_compare_exchange_weak(&is_locked, &UNLOCKED, LOCKED));
       }
 
-      void unlock() const
+      void unlock()
       {
         //if unlock is called when it is already unlocked then behaviour is undefined.
         std::atomic_compare_exchange_strong(&is_locked, &LOCKED, UNLOCKED);
       }
 
     private:
-      mutable std::atomic_bool is_locked = false;
+      std::atomic_bool is_locked = false;
   };
 } // namespace sp
