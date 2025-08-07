f051487ad1f3c1f8fd5056dc8e71b128f0d0ecd6
breakbadsp <sachinadcet@gmail.com>
2025-08-07 07:17:35 +0530

    Moved GetIpPort to cpp

--- sp/cmn.hpp
+++ sp/cmn.hpp
@@ -15,21 +15,7 @@
 
 namespace sp 
 {
-
-  std::optional<std::pair<std::string, short>> GetIpPort(const struct sockaddr& p_cli_addr)
-{
-  char ip[INET6_ADDRSTRLEN];
-  if(p_cli_addr.sa_family == AF_INET)
-  {
-    const struct sockaddr_in& addr_in = (const struct sockaddr_in&) (p_cli_addr);
-    const char* ip1 = inet_ntop(AF_INET, &(addr_in.sin_addr), ip, INET_ADDRSTRLEN);
-    if(ip1 != nullptr)
-    {
-      return {std::pair(ip1, ntohs(addr_in.sin_port))};
-    }
-  }
-  return {};
-}
+  std::optional<std::pair<std::string, short>> GetIpPort(const struct sockaddr& p_cli_addr);
 
 inline auto SetThreadCore(int p_core_id)
 {
